c423f635af3c49fccdde2399613a4c47b92b7c4d
aviv <avivmaor9@gmail.com>
2025-06-12 15:16:21 +0300

    Revert "First commit"
    This reverts commit faa87ef9ae4c894c945d5da87d64fe2d1db9f3dc.

--- app/src/main/java/com/example/securenotes/di/IoDispatcher.kt
+++
@@ -1,9 +0,0 @@
-package com.example.securenotes.di
-
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-import javax.inject.Qualifier
-
-@Retention(RetentionPolicy.RUNTIME)
-@Qualifier
-annotation class IoDispatcher
