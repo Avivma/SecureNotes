3d671e7f3eb0011c821eff125065d23cf30cb9a0
aviv <avivmaor9@gmail.com>
2025-06-26 15:07:57 +0300

    convert gradle files from Groovy language to Kotlin (.kts extension)

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
+    }
+}
+
+allprojects {
+    repositories {
+//        google()
+//        mavenCentral()
+    }
+}
